8fcca8a9e0a46d121f2514168b7a9cfe8479f4c9
khalidwaleedkhedr <khalid.w.khedr@gmail.com>
2025-01-31 02:36:01 -0500

    Fixing / Updating / Making controller for Ebay API window. Some Bugs remaining to fix such as Dropdown box. It's all just controllers and services from here.

--- gui/gui_form_ebay.py
+++ gui/gui_form_ebay.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ui_ebayrJdxwV.ui'
+## Form generated from reading UI file 'ui_ebayzQJvGb.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.1
 ##
@@ -23,68 +23,74 @@ class Ui_form_EbayAPI(object):
     def setupUi(self, form_EbayAPI):
         if not form_EbayAPI.objectName():
             form_EbayAPI.setObjectName(u"form_EbayAPI")
-        form_EbayAPI.resize(617, 331)
+        form_EbayAPI.resize(531, 286)
         self.frame = QFrame(form_EbayAPI)
         self.frame.setObjectName(u"frame")
-        self.frame.setGeometry(QRect(120, 50, 451, 221))
+        self.frame.setGeometry(QRect(50, 20, 411, 201))
+        self.frame.setMinimumSize(QSize(411, 201))
         self.frame.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
-        self.gridLayout_2 = QGridLayout(self.frame)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout = QGridLayout()
+        self.gridLayout = QGridLayout(self.frame)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.comboBox_SITE_ID = QComboBox(self.frame)
-        self.comboBox_SITE_ID.setObjectName(u"comboBox_SITE_ID")
-
-        self.gridLayout.addWidget(self.comboBox_SITE_ID, 0, 2, 1, 1)
-
-        self.button_IMPORT_CONFIG = QPushButton(self.frame)
-        self.button_IMPORT_CONFIG.setObjectName(u"button_IMPORT_CONFIG")
-
-        self.gridLayout.addWidget(self.button_IMPORT_CONFIG, 3, 2, 1, 1)
-
-        self.label_domain = QLabel(self.frame)
-        self.label_domain.setObjectName(u"label_domain")
-
-        self.gridLayout.addWidget(self.label_domain, 2, 0, 1, 1)
-
         self.label_site_id = QLabel(self.frame)
         self.label_site_id.setObjectName(u"label_site_id")
+        self.label_site_id.setMinimumSize(QSize(249, 33))
 
         self.gridLayout.addWidget(self.label_site_id, 0, 0, 1, 2)
 
-        self.text_Domain = QTextEdit(self.frame)
-        self.text_Domain.setObjectName(u"text_Domain")
+        self.comboBox_SITE_ID = QComboBox(self.frame)
+        self.comboBox_SITE_ID.setObjectName(u"comboBox_SITE_ID")
+        self.comboBox_SITE_ID.setMinimumSize(QSize(142, 33))
 
-        self.gridLayout.addWidget(self.text_Domain, 2, 1, 1, 2)
+        self.gridLayout.addWidget(self.comboBox_SITE_ID, 0, 2, 1, 1)
 
         self.label_app_id = QLabel(self.frame)
         self.label_app_id.setObjectName(u"label_app_id")
+        self.label_app_id.setMinimumSize(QSize(122, 33))
 
         self.gridLayout.addWidget(self.label_app_id, 1, 0, 1, 1)
 
-        self.button_CONNECT = QPushButton(self.frame)
-        self.button_CONNECT.setObjectName(u"button_CONNECT")
-
-        self.gridLayout.addWidget(self.button_CONNECT, 4, 2, 1, 1)
-
-        self.label_config_file = QLabel(self.frame)
-        self.label_config_file.setObjectName(u"label_config_file")
-
-        self.gridLayout.addWidget(self.label_config_file, 3, 0, 1, 2)
-
-        self.pushButton = QPushButton(self.frame)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.gridLayout.addWidget(self.pushButton, 4, 0, 1, 2)
-
         self.text_AppID = QTextEdit(self.frame)
         self.text_AppID.setObjectName(u"text_AppID")
+        self.text_AppID.setMinimumSize(QSize(269, 33))
 
         self.gridLayout.addWidget(self.text_AppID, 1, 1, 1, 2)
 
+        self.label_domain = QLabel(self.frame)
+        self.label_domain.setObjectName(u"label_domain")
+        self.label_domain.setMinimumSize(QSize(122, 32))
 
-        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_domain, 2, 0, 1, 1)
+
+        self.text_Domain = QTextEdit(self.frame)
+        self.text_Domain.setObjectName(u"text_Domain")
+        self.text_Domain.setMinimumSize(QSize(269, 32))
+
+        self.gridLayout.addWidget(self.text_Domain, 2, 1, 1, 2)
+
+        self.label_config_file = QLabel(self.frame)
+        self.label_config_file.setObjectName(u"label_config_file")
+        self.label_config_file.setMinimumSize(QSize(122, 33))
+
+        self.gridLayout.addWidget(self.label_config_file, 3, 0, 1, 1)
+
+        self.button_IMPORT_CONFIG = QPushButton(self.frame)
+        self.button_IMPORT_CONFIG.setObjectName(u"button_IMPORT_CONFIG")
+        self.button_IMPORT_CONFIG.setMinimumSize(QSize(142, 33))
+
+        self.gridLayout.addWidget(self.button_IMPORT_CONFIG, 3, 2, 1, 1)
+
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setMinimumSize(QSize(122, 32))
+
+        self.gridLayout.addWidget(self.pushButton, 4, 0, 1, 1)
+
+        self.button_CONNECT = QPushButton(self.frame)
+        self.button_CONNECT.setObjectName(u"button_CONNECT")
+        self.button_CONNECT.setMinimumSize(QSize(142, 32))
+
+        self.gridLayout.addWidget(self.button_CONNECT, 4, 2, 1, 1)
 
 
         self.retranslateUi(form_EbayAPI)
@@ -94,12 +100,12 @@ class Ui_form_EbayAPI(object):
 
     def retranslateUi(self, form_EbayAPI):
         form_EbayAPI.setWindowTitle(QCoreApplication.translate("form_EbayAPI", u"Ebay API Settings", None))
-        self.button_IMPORT_CONFIG.setText(QCoreApplication.translate("form_EbayAPI", u"Import Config", None))
-        self.label_domain.setText(QCoreApplication.translate("form_EbayAPI", u"DOMAIN:", None))
         self.label_site_id.setText(QCoreApplication.translate("form_EbayAPI", u"SITE ID:", None))
         self.label_app_id.setText(QCoreApplication.translate("form_EbayAPI", u"APP ID:", None))
-        self.button_CONNECT.setText(QCoreApplication.translate("form_EbayAPI", u"Connect", None))
+        self.label_domain.setText(QCoreApplication.translate("form_EbayAPI", u"DOMAIN:", None))
         self.label_config_file.setText(QCoreApplication.translate("form_EbayAPI", u"CONFIG FILE:", None))
+        self.button_IMPORT_CONFIG.setText(QCoreApplication.translate("form_EbayAPI", u"Import Config", None))
         self.pushButton.setText(QCoreApplication.translate("form_EbayAPI", u"Test API", None))
+        self.button_CONNECT.setText(QCoreApplication.translate("form_EbayAPI", u"Connect", None))
     # retranslateUi
 
