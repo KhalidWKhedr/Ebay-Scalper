632eeeda5a67fe68d3c08be43399ba3e957a3ea9
khalidwaleedkhedr <khalid.w.khedr@gmail.com>
2025-01-31 03:25:19 -0500

    Fixing / Updating / Making controller for Ebay API window. Some Bugs remaining to fix such as Dropdown box. It's all just controllers and services from here.

--- gui/gui_form_database.py
+++ gui/gui_form_database.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ui_databasebMRplk.ui'
+## Form generated from reading UI file 'ui_databaserKkLpa.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.1
 ##
@@ -26,50 +26,60 @@ class Ui_form_Database(object):
         form_Database.resize(1039, 661)
         self.frame_MongoDB = QFrame(form_Database)
         self.frame_MongoDB.setObjectName(u"frame_MongoDB")
-        self.frame_MongoDB.setGeometry(QRect(190, 200, 441, 241))
+        self.frame_MongoDB.setGeometry(QRect(170, 20, 441, 241))
         self.frame_MongoDB.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame_MongoDB.setFrameShadow(QFrame.Shadow.Raised)
         self.gridLayout_2 = QGridLayout(self.frame_MongoDB)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.text_Host = QTextEdit(self.frame_MongoDB)
-        self.text_Host.setObjectName(u"text_Host")
-
-        self.gridLayout_2.addWidget(self.text_Host, 1, 1, 1, 1)
-
-        self.label_mongocredentials = QLabel(self.frame_MongoDB)
-        self.label_mongocredentials.setObjectName(u"label_mongocredentials")
-
-        self.gridLayout_2.addWidget(self.label_mongocredentials, 0, 1, 1, 1)
-
         self.label_dbname = QLabel(self.frame_MongoDB)
         self.label_dbname.setObjectName(u"label_dbname")
 
         self.gridLayout_2.addWidget(self.label_dbname, 6, 0, 1, 1)
-
-        self.text_AuthSource = QTextEdit(self.frame_MongoDB)
-        self.text_AuthSource.setObjectName(u"text_AuthSource")
-
-        self.gridLayout_2.addWidget(self.text_AuthSource, 5, 1, 1, 1)
-
-        self.label_port = QLabel(self.frame_MongoDB)
-        self.label_port.setObjectName(u"label_port")
-
-        self.gridLayout_2.addWidget(self.label_port, 2, 0, 1, 1)
 
         self.text_Username = QTextEdit(self.frame_MongoDB)
         self.text_Username.setObjectName(u"text_Username")
 
         self.gridLayout_2.addWidget(self.text_Username, 3, 1, 1, 1)
 
-        self.text_Port = QTextEdit(self.frame_MongoDB)
-        self.text_Port.setObjectName(u"text_Port")
-
-        self.gridLayout_2.addWidget(self.text_Port, 2, 1, 1, 1)
-
         self.text_DbName = QTextEdit(self.frame_MongoDB)
         self.text_DbName.setObjectName(u"text_DbName")
 
         self.gridLayout_2.addWidget(self.text_DbName, 6, 1, 1, 1)
+
+        self.label_username = QLabel(self.frame_MongoDB)
+        self.label_username.setObjectName(u"label_username")
+
+        self.gridLayout_2.addWidget(self.label_username, 3, 0, 1, 1)
+
+        self.text_Password = QTextEdit(self.frame_MongoDB)
+        self.text_Password.setObjectName(u"text_Password")
+
+        self.gridLayout_2.addWidget(self.text_Password, 4, 1, 1, 1)
+
+        self.text_Host = QTextEdit(self.frame_MongoDB)
+        self.text_Host.setObjectName(u"text_Host")
+
+        self.gridLayout_2.addWidget(self.text_Host, 1, 1, 1, 1)
+
+        self.label_port = QLabel(self.frame_MongoDB)
+        self.label_port.setObjectName(u"label_port")
+
+        self.gridLayout_2.addWidget(self.label_port, 2, 0, 1, 1)
+
+        self.label_host = QLabel(self.frame_MongoDB)
+        self.label_host.setObjectName(u"label_host")
+
+        self.gridLayout_2.addWidget(self.label_host, 1, 0, 1, 1)
+
+        self.text_AuthSource = QTextEdit(self.frame_MongoDB)
+        self.text_AuthSource.setObjectName(u"text_AuthSource")
+
+        self.gridLayout_2.addWidget(self.text_AuthSource, 5, 1, 1, 1)
+
+        self.text_Port = QTextEdit(self.frame_MongoDB)
+        self.text_Port.setObjectName(u"text_Port")
+
+        self.gridLayout_2.addWidget(self.text_Port, 2, 1, 1, 1)
 
         self.label_authsource = QLabel(self.frame_MongoDB)
         self.label_authsource.setObjectName(u"label_authsource")
@@ -81,80 +91,70 @@ class Ui_form_Database(object):
 
         self.gridLayout_2.addWidget(self.label_password, 4, 0, 1, 1)
 
-        self.label_username = QLabel(self.frame_MongoDB)
-        self.label_username.setObjectName(u"label_username")
+        self.label_mongocredentials = QLabel(self.frame_MongoDB)
+        self.label_mongocredentials.setObjectName(u"label_mongocredentials")
 
-        self.gridLayout_2.addWidget(self.label_username, 3, 0, 1, 1)
-
-        self.label_host = QLabel(self.frame_MongoDB)
-        self.label_host.setObjectName(u"label_host")
-
-        self.gridLayout_2.addWidget(self.label_host, 1, 0, 1, 1)
-
-        self.text_Password = QTextEdit(self.frame_MongoDB)
-        self.text_Password.setObjectName(u"text_Password")
-
-        self.gridLayout_2.addWidget(self.text_Password, 4, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.label_mongocredentials, 0, 1, 1, 1)
 
         self.button_Connect = QPushButton(form_Database)
         self.button_Connect.setObjectName(u"button_Connect")
-        self.button_Connect.setGeometry(QRect(670, 490, 114, 36))
+        self.button_Connect.setGeometry(QRect(740, 580, 114, 36))
         self.frame_SSH = QFrame(form_Database)
         self.frame_SSH.setObjectName(u"frame_SSH")
-        self.frame_SSH.setGeometry(QRect(190, 20, 441, 181))
+        self.frame_SSH.setGeometry(QRect(170, 260, 441, 181))
         self.frame_SSH.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame_SSH.setFrameShadow(QFrame.Shadow.Raised)
         self.gridLayout = QGridLayout(self.frame_SSH)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.text_SSH_Username = QTextEdit(self.frame_SSH)
-        self.text_SSH_Username.setObjectName(u"text_SSH_Username")
+        self.text_SSH_Password = QTextEdit(self.frame_SSH)
+        self.text_SSH_Password.setObjectName(u"text_SSH_Password")
 
-        self.gridLayout.addWidget(self.text_SSH_Username, 3, 1, 1, 1)
-
-        self.label_ssh_password = QLabel(self.frame_SSH)
-        self.label_ssh_password.setObjectName(u"label_ssh_password")
-
-        self.gridLayout.addWidget(self.label_ssh_password, 4, 0, 1, 1)
-
-        self.label_ssh_host = QLabel(self.frame_SSH)
-        self.label_ssh_host.setObjectName(u"label_ssh_host")
-
-        self.gridLayout.addWidget(self.label_ssh_host, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.text_SSH_Password, 4, 1, 1, 1)
 
         self.label_ssh_username = QLabel(self.frame_SSH)
         self.label_ssh_username.setObjectName(u"label_ssh_username")
 
         self.gridLayout.addWidget(self.label_ssh_username, 3, 0, 1, 1)
 
+        self.text_SSH_Port = QTextEdit(self.frame_SSH)
+        self.text_SSH_Port.setObjectName(u"text_SSH_Port")
+
+        self.gridLayout.addWidget(self.text_SSH_Port, 2, 1, 1, 1)
+
+        self.label_ssh_password = QLabel(self.frame_SSH)
+        self.label_ssh_password.setObjectName(u"label_ssh_password")
+
+        self.gridLayout.addWidget(self.label_ssh_password, 4, 0, 1, 1)
+
         self.label_ssh_port = QLabel(self.frame_SSH)
         self.label_ssh_port.setObjectName(u"label_ssh_port")
 
         self.gridLayout.addWidget(self.label_ssh_port, 2, 0, 1, 1)
-
-        self.label_sshcredentials = QLabel(self.frame_SSH)
-        self.label_sshcredentials.setObjectName(u"label_sshcredentials")
-
-        self.gridLayout.addWidget(self.label_sshcredentials, 0, 1, 1, 1)
 
         self.checkbox_SSH = QCheckBox(self.frame_SSH)
         self.checkbox_SSH.setObjectName(u"checkbox_SSH")
 
         self.gridLayout.addWidget(self.checkbox_SSH, 0, 0, 1, 1)
 
+        self.text_SSH_Username = QTextEdit(self.frame_SSH)
+        self.text_SSH_Username.setObjectName(u"text_SSH_Username")
+
+        self.gridLayout.addWidget(self.text_SSH_Username, 3, 1, 1, 1)
+
         self.text_SSH_Host = QTextEdit(self.frame_SSH)
         self.text_SSH_Host.setObjectName(u"text_SSH_Host")
 
         self.gridLayout.addWidget(self.text_SSH_Host, 1, 1, 1, 1)
 
-        self.text_SSH_Password = QTextEdit(self.frame_SSH)
-        self.text_SSH_Password.setObjectName(u"text_SSH_Password")
+        self.label_ssh_host = QLabel(self.frame_SSH)
+        self.label_ssh_host.setObjectName(u"label_ssh_host")
 
-        self.gridLayout.addWidget(self.text_SSH_Password, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.label_ssh_host, 1, 0, 1, 1)
 
-        self.text_SSH_Port = QTextEdit(self.frame_SSH)
-        self.text_SSH_Port.setObjectName(u"text_SSH_Port")
+        self.label_sshcredentials = QLabel(self.frame_SSH)
+        self.label_sshcredentials.setObjectName(u"label_sshcredentials")
 
-        self.gridLayout.addWidget(self.text_SSH_Port, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.label_sshcredentials, 0, 1, 1, 1)
 
         self.frame_auth = QFrame(form_Database)
         self.frame_auth.setObjectName(u"frame_auth")
@@ -163,10 +163,20 @@ class Ui_form_Database(object):
         self.frame_auth.setFrameShadow(QFrame.Shadow.Raised)
         self.gridLayout_3 = QGridLayout(self.frame_auth)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.radio_SHA1 = QRadioButton(self.frame_auth)
+        self.radio_SHA1.setObjectName(u"radio_SHA1")
+
+        self.gridLayout_3.addWidget(self.radio_SHA1, 1, 0, 1, 1)
+
         self.radio_AWS = QRadioButton(self.frame_auth)
         self.radio_AWS.setObjectName(u"radio_AWS")
 
         self.gridLayout_3.addWidget(self.radio_AWS, 5, 0, 1, 1)
+
+        self.r = QRadioButton(self.frame_auth)
+        self.r.setObjectName(u"r")
+
+        self.gridLayout_3.addWidget(self.r, 7, 0, 1, 1)
 
         self.radio_X509 = QRadioButton(self.frame_auth)
         self.radio_X509.setObjectName(u"radio_X509")
@@ -188,16 +198,6 @@ class Ui_form_Database(object):
 
         self.gridLayout_3.addWidget(self.radio_LDAP, 3, 0, 1, 1)
 
-        self.r = QRadioButton(self.frame_auth)
-        self.r.setObjectName(u"r")
-
-        self.gridLayout_3.addWidget(self.r, 7, 0, 1, 1)
-
-        self.radio_SHA1 = QRadioButton(self.frame_auth)
-        self.radio_SHA1.setObjectName(u"radio_SHA1")
-
-        self.gridLayout_3.addWidget(self.radio_SHA1, 1, 0, 1, 1)
-
         self.label_6 = QLabel(self.frame_auth)
         self.label_6.setObjectName(u"label_6")
 
@@ -205,7 +205,7 @@ class Ui_form_Database(object):
 
         self.button_TestConnection = QPushButton(form_Database)
         self.button_TestConnection.setObjectName(u"button_TestConnection")
-        self.button_TestConnection.setGeometry(QRect(160, 460, 161, 36))
+        self.button_TestConnection.setGeometry(QRect(170, 610, 161, 36))
 
         self.retranslateUi(form_Database)
 
@@ -214,27 +214,27 @@ class Ui_form_Database(object):
 
     def retranslateUi(self, form_Database):
         form_Database.setWindowTitle(QCoreApplication.translate("form_Database", u"Database Configuration Settings", None))
-        self.label_mongocredentials.setText(QCoreApplication.translate("form_Database", u"MongoDB Credentials:", None))
         self.label_dbname.setText(QCoreApplication.translate("form_Database", u"DB NAME:", None))
+        self.label_username.setText(QCoreApplication.translate("form_Database", u"USERNAME:", None))
         self.label_port.setText(QCoreApplication.translate("form_Database", u"PORT:", None))
+        self.label_host.setText(QCoreApplication.translate("form_Database", u"HOST:", None))
         self.label_authsource.setText(QCoreApplication.translate("form_Database", u"AUTH SOURCE:", None))
         self.label_password.setText(QCoreApplication.translate("form_Database", u"PASSWORD:", None))
-        self.label_username.setText(QCoreApplication.translate("form_Database", u"USERNAME:", None))
-        self.label_host.setText(QCoreApplication.translate("form_Database", u"HOST:", None))
+        self.label_mongocredentials.setText(QCoreApplication.translate("form_Database", u"MongoDB Credentials:", None))
         self.button_Connect.setText(QCoreApplication.translate("form_Database", u"Connect", None))
-        self.label_ssh_password.setText(QCoreApplication.translate("form_Database", u"SSH PASSWORD:", None))
-        self.label_ssh_host.setText(QCoreApplication.translate("form_Database", u"SSH HOST:", None))
         self.label_ssh_username.setText(QCoreApplication.translate("form_Database", u"SSH USERNAME:", None))
+        self.label_ssh_password.setText(QCoreApplication.translate("form_Database", u"SSH PASSWORD:", None))
         self.label_ssh_port.setText(QCoreApplication.translate("form_Database", u"SSH PORT:", None))
-        self.label_sshcredentials.setText(QCoreApplication.translate("form_Database", u"SSH Credentials", None))
         self.checkbox_SSH.setText(QCoreApplication.translate("form_Database", u"SSH", None))
+        self.label_ssh_host.setText(QCoreApplication.translate("form_Database", u"SSH HOST:", None))
+        self.label_sshcredentials.setText(QCoreApplication.translate("form_Database", u"SSH Credentials", None))
+        self.radio_SHA1.setText(QCoreApplication.translate("form_Database", u"SCRAM-SHA-&1  ", None))
         self.radio_AWS.setText(QCoreApplication.translate("form_Database", u"MO&NGODB-AWS  ", None))
+        self.r.setText(QCoreApplication.translate("form_Database", u"P&LAIN  ", None))
         self.radio_X509.setText(QCoreApplication.translate("form_Database", u"MONGODB-&X509  ", None))
         self.radio_KERBEROS.setText(QCoreApplication.translate("form_Database", u"&GSSAPI (Kerberos)  ", None))
         self.radio_SHA256.setText(QCoreApplication.translate("form_Database", u"SCRAM-SHA-&256  ", None))
         self.radio_LDAP.setText(QCoreApplication.translate("form_Database", u"L&DAP", None))
-        self.r.setText(QCoreApplication.translate("form_Database", u"P&LAIN  ", None))
-        self.radio_SHA1.setText(QCoreApplication.translate("form_Database", u"SCRAM-SHA-&1  ", None))
         self.label_6.setText(QCoreApplication.translate("form_Database", u"Authentication Types:", None))
         self.button_TestConnection.setText(QCoreApplication.translate("form_Database", u"Test Connection", None))
     # retranslateUi
