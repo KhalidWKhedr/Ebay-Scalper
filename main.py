a9c9068d629ee7228f7984929f558f12af55abc4
khalidwaleedkhedr <khalid.w.khedr@gmail.com>
2025-02-01 00:12:18 -0500

    Fixing / Updating / Making controller for Ebay API window. Some Bugs remaining to fix such as Dropdown box. It's all just controllers and services from here.

--- main.py
+++ main.py
@@ -2,26 +2,8 @@ import sys
 
 # from core import EbayItemFetcher
 from PySide6.QtWidgets import QApplication
-from sshtunnel import SSHTunnelForwarder
 
 from controllers.controller_main import MainController
-
-def create_tunnel():
-    """Creates the SSH tunnel."""
-    try:
-        tunnel = SSHTunnelForwarder(
-            ('192.168.120', 22),
-            ssh_username='Khalid',
-            ssh_password='Fuckrtu@1',
-            remote_bind_address=('localhost', 27017),
-            local_bind_address=("localhost", 27018)  # Local port for the SSH tunnel
-        )
-        tunnel.start()
-        print("SSH tunnel established.")
-    except Exception as e:
-        print(f"Error establishing SSH tunnel: {e}")
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # Create the application
