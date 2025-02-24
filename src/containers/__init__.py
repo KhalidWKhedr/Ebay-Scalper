0c9dfb5a3145a0ab33e3c495b9a06df35cd95d16
khalidwaleedkhedr <khalid.w.khedr@gmail.com>
2025-02-24 11:48:59 -0500

    Save Connection Settings -> Updating Logic and proper testing, making more seamless logic.

--- src/containers/__init__.py
+++ src/containers/__init__.py
@@ -3,7 +3,7 @@ from .container_utils import UtilsContainer
 from .container_infrastructure import InfrastructureContainer
 from .container_services import ServicesContainer
 from .container_controllers import ControllersContainer
-from .container_presenters import PresentersContainer
+from src.containers.presenters.container_presenters import PresentersContainer
 from .container_core import CoreContainer
 from .container_application import ApplicationContainer
 
--- src/containers/presenters/container_presenters.py
+++ src/containers/presenters/container_presenters.py
@@ -2,8 +2,8 @@ from dependency_injector import containers, providers
 from src.view.presenters.form_main.presenter_main import MainPresenter
 from src.view.gui.gui_form_main import Ui_form_MainWindow
 from src.view.presenters.form_main.presenter_csv import CsvPresenter
-from .container_controllers import ControllersContainer
-from .container_services import ServicesContainer
+from src.containers.container_controllers import ControllersContainer
+from src.containers.container_services import ServicesContainer
 
 
 class PresentersContainer(containers.DeclarativeContainer):
